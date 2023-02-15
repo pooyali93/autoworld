b78801f02b00e79bc3a8c5c264f7904142bb4552
pooyali93 <k2049963@gmail.com>
2023-02-15 00:15:24 +0000

    Banner

---
+++ src/components/UI/HeroBanner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+import Slider from 'react-slick'
+
+const HeroBanner = () => {
+
+  return (
+   <Slider className='hero_banner'>
+    
+    .
+
+   </Slider>
+  )
+}
+
+export default HeroBanner
